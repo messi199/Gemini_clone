0f1c36d8a4db99bd8a1cb8ae9b88c8b3c51e3d97
messi199 <mesfing1717@gmail.com>
2025-04-07 16:18:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+import './components/Sidebar/Sidebar.css'
+import './index.css'
+import Main from './components/Main/Main'
+
+
+
+const App = () => {
+  return (
+    <>
+    <Sidebar />
+    <Main />
+
+    </> 
+  )
+}
+
+export default App
